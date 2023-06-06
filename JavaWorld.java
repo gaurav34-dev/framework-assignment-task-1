81b67221c133bc6419915ffb2b9979998e7fd066
gbaser54 <68541784+gbaser54@users.noreply.github.com>
2023-06-06 17:55:23 -0400

    Create JavaWorld.java

---
+++ JavaWorld.java
@@ -0,0 +1,11 @@
+//avaWorld class implements the JavaLanguage interface
+
+package com.example;
+
+public class JavaWorld implements JavaLanguage {
+
+    @Override
+    public String learningJava() {
+        return "I am learning java";
+    }
+}
